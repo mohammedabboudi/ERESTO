a8720fcfb8ef5c73b582687a38b3d299fdd01882
mohammedabboudi <mohammedabboudi10@gmail.com>
2022-04-10 13:51:40 +0000

    make an user endpoint using router

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const database = require('./app/config/database/Mongoose_connection')
+const userRouter = require('./app/api/routes/User')
+
 const port = process.env.PORT || 3000;
+
 const app = express();
 
-app.listen(port, ()=>{ console.log(`WE ARE CONNECTED TO THE SERVER SUCCESSFULY...`) });
+app.use('/user', userRouter);
+
+app.listen(port, ()=>{ console.log(`WE ARE CONNECTED TO THE SERVER SUCCESSFULY ON THE PORT : ${port}`) });
