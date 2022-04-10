cb78fd915d407c2d2914d2fbfdea838a951f3189
mohammedabboudi <mohammedabboudi10@gmail.com>
2022-04-10 21:10:00 +0000

    create middleware for userValidation using express-validator

--- app/api/routes/User.js
+++ app/api/routes/User.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/User')
+const userController = require('../controllers/User');
+const userMiddleware = require('../middleware/User');
+
+
+
+
 
 router.get('/', userController.userList);
+
+router.post('/register',userMiddleware.validator, userMiddleware.userValidator, userController.userRegister);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const database = require('./app/config/database/Mongoose_connection')
 const userRouter = require('./app/api/routes/User')
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
 
 app.use('/user', userRouter);
 
