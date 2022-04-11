7dbfbe0d1a710205801fa756f8c53d0d6f096825
mohammedabboudi <mohammedabboudi10@gmail.com>
2022-04-11 17:06:40 +0000

    create login route

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const database = require('./app/config/database/Mongoose_connection')
-const userRouter = require('./app/api/routes/User')
 const bodyParser = require('body-parser');
+const userRouter = require('./app/api/routes/User');
+const loginRouter = require('./app/api/routes/Login');
 
 const port = process.env.PORT || 3000;
 
@@ -12,5 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
 app.use('/user', userRouter);
+
+app.use('/', loginRouter);
 
 app.listen(port, ()=>{ console.log(`WE ARE CONNECTED TO THE SERVER SUCCESSFULY ON THE PORT : ${port}`) });
