14729935c80c9667372ee4a0c71f73bae4a960a6
mohammedabboudi <mohammedabboudi10@gmail.com>
2022-04-20 14:59:31 +0000

    create delete restaurant route and controller

--- app/api/routes/manager.js
+++ app/api/routes/manager.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addRestaurant, editRestaurant } = require('../controllers/manager');
+const { addRestaurant, editRestaurant, deleteRestaurant } = require('../controllers/manager');
 const { authorization } = require('../middleware/authorizeJWTs');
 const { checkRole } = require('../middleware/checkRole');
 const { restaurantValidator, restaurantValidation } = require('../validations/restaurant-validation');
@@ -9,6 +9,7 @@ const role = 'manager';
 
 router.post('/restaurant/add', authorization, checkRole(role), restaurantValidator, restaurantValidation, addRestaurant);
 router.patch('/restaurant/edit', authorization, checkRole(role), restaurantValidator, restaurantValidation, editRestaurant);
+router.delete('/restaurant/delete', authorization, checkRole(role), deleteRestaurant);
 
 
 
