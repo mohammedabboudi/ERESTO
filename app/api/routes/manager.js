966551e1123a0c242dc4dd97b0bf534c36cc2ca8
mohammedabboudi <mohammedabboudi10@gmail.com>
2022-04-21 11:26:42 +0000

    add meal validation to add meal route

--- app/api/routes/manager.js
+++ app/api/routes/manager.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { addRestaurant, editRestaurant, deleteRestaurant, addMeal } = require('../controllers/manager');
 const { authorization } = require('../middleware/authorizeJWTs');
 const { checkRole } = require('../middleware/checkRole');
+const { mealValidator, mealValidation } = require('../validations/meal-validation');
 const { restaurantValidator, restaurantValidation } = require('../validations/restaurant-validation');
 const router = express.Router();
 
@@ -10,7 +11,7 @@ const role = 'manager';
 router.post('/restaurant/add', authorization, checkRole(role), restaurantValidator, restaurantValidation, addRestaurant);
 router.patch('/restaurant/edit', authorization, checkRole(role), restaurantValidator, restaurantValidation, editRestaurant);
 router.delete('/restaurant/delete', authorization, checkRole(role), deleteRestaurant);
-router.post('/meal/add', authorization, checkRole(role), addMeal);
+router.post('/meal/add', authorization, checkRole(role), mealValidator, mealValidation, addMeal);
 
 
 
--- app/api/validations/meal-validation.js
+++ app/api/validations/meal-validation.js
@@ -4,9 +4,11 @@ const { body , validationResult } = require('express-validator');
 
 const mealValidator = [
     body('name').isLength({ min : 2 }).withMessage('the name is required'),
-    body('manager').isLength({ min : 2 }).withMessage('phone manager must be at least 2 chars long'),
-    body('location').isLength({ min : 2 }).withMessage('location must be at least 2 chars long'),
-    body('city').isLength({ min : 2 }).withMessage('city must be at least 4 chars long')
+    body('description').isLength({ min : 2 }).withMessage('phone manager must be at least 2 chars long'),
+    body('price').isLength({ min : 2 }).withMessage('location must be at least 2 chars long'),
+    body('image').isLength({ min : 2 }).withMessage('location must be at least 2 chars long'),
+    body('restaurant').isLength({ min : 2 }).withMessage('location must be at least 2 chars long'),
+    body('category').isLength({ min : 2 }).withMessage('city must be at least 4 chars long')
  ]
 
 
